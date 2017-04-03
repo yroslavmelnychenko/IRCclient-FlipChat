5217b89b9ee2448cd5cbda8cdd31aeb51d4e3d1a
Yroslav Melnychenko <y.melnychenko@zoho.com>
2017-04-03 17:29:46 +0300

    first commit

---
+++ ircClient-FlipChat/Model/ChatMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ircClient_FlipChat.Model
+{
+    public class ChatMessage
+    {
+       
+        public DateTime Time { get; set; }
+        public string Nick { get; set; }
+        public string Message { get; set; }               
+       
+          public ChatMessage()
+        {
+            Time = DateTime.Now;
+            Nick = "kut";
+            Message = "super chat kut";
+        }
+        
+    }
+}
---
+++ ircClient-FlipChat/ViewModels/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ircClient_FlipChat.Model;
+
+namespace ircClient_FlipChat.ViewModels 
+{
+    public class ViewModel : ViewModelBase
+    {
+
+    }
+}
